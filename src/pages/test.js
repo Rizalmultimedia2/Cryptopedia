e0ef6ca66187cd4de91a718ac22c7d4346779d94
Rizal Herliansyah Hidayat <39128288+Rizalmultimedia2@users.noreply.github.com>
2023-05-09 23:10:08 +0700

    Push test

--- src/pages/test.js
+++ src/pages/test.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import { Menu } from "@headlessui/react";
 
 function test() {
   return (
-    <div>testaaaa</div>
-  )
+    <div className="grid grid-cols-2 container">
+      <div></div>
+      <div className="flex items-center justify-center min-h-screen">
+        <div className="flex h-[537px] w-[486px] bg-light-color rounded-md shadow-shadows-1 items-center justify-center">
+          <h1 className="text-primary-1 text-lg">Masuk</h1>
+          <p className="text-primary-1 font-extrabold"></p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default test
+export default test;
