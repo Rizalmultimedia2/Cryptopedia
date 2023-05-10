441c10cb29400f176b97a9d02e9c356063f52b08
Rizalmultimedia2 <rizalmultimedia2@gmail.com>
2023-05-10 19:56:51 +0700

    push

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,32 +1,31 @@
+import Image from "next/image";
 import React from "react";
 
 function Header() {
   return (
     <header>
-      <nav class="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
+      <nav class="bg-white border-gray-200 px-4 lg:px-6 py-2.5 shadow-shadows-1">
         <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
-          <a href="https://flowbite.com" class="flex items-center">
-            <img
-              src="https://flowbite.com/docs/images/logo.svg"
-              class="mr-3 h-6 sm:h-9"
-              alt="Flowbite Logo"
+          <a href="#" class="flex items-center">
+            <Image
+              src="/image/logo.png"
+              height={50}
+              width={55}
+              alt="Cryptopedia"
             />
-            <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
-              Flowbite
-            </span>
           </a>
           <div class="flex items-center lg:order-2">
             <a
               href="#"
-              class="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800"
+              class="text-primary-1 hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 border border-primary-1 focus:outline-none"
             >
-              Log in
+              Daftar
             </a>
             <a
               href="#"
               class="text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800"
             >
-              Get started
+              Masuk
             </a>
             <button
               data-collapse-toggle="mobile-menu-2"
@@ -70,7 +69,7 @@ function Header() {
               <li>
                 <a
                   href="#"
-                  class="block py-2 pr-4 pl-3 text-white rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-white"
+                  class="block py-2 pr-4 pl-3 text-black rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 hover:text-primary-1"
                   aria-current="page"
                 >
                   Home
@@ -79,9 +78,17 @@ function Header() {
               <li>
                 <a
                   href="#"
-                  class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
+                  class="block py-2 pr-4 pl-3 text-black border-b border-gray-100 hover:text-primary-1 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0"
                 >
-                  Company
+                  Artikel
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0"
+                >
+                  Crypto Sharing
                 </a>
               </li>
               <li>
@@ -89,7 +96,7 @@ function Header() {
                   href="#"
                   class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
                 >
-                  Marketplace
+                  Crypto 101
                 </a>
               </li>
               <li>
@@ -97,23 +104,7 @@ function Header() {
                   href="#"
                   class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
                 >
-                  Features
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
-                >
-                  Team
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
-                >
-                  Contact
+                  Tentang
                 </a>
               </li>
             </ul>
--- src/pages/masuk.js
+++ src/pages/masuk.js
@@ -9,19 +9,13 @@ function test() {
       <Header />
       <div className="grid md:grid-cols-2 grid-cols-1 container">
         <div></div>
-        <div className="test">
-          <div className="flex md:flex-col gap-[25px] py-[35px] w-[486px] bg-light-color rounded-md shadow-shadows-1 items-center justify-center space-y-5">
+        <div className="flex items-center justify-center min-h-screen">
+          <div className="flex md:flex-col gap-[20px] py-[35px] px-[15px] w-[486px] bg-light-color rounded-md shadow-shadows-1 items-center justify-center space-y-5">
             <Image src="/image/logo.png" height={120} width={133} />
             <h1 className="text-primary-1 text-h5">Masuk</h1>
-            <div className="flex items-center h-[45px] font-medium w-[420px] border border-gray-400 rounded-lg text-p3 px-3">
-              Username
-            </div>
-            <div className="flex items-center h-[45px] font-medium w-[420px] border border-gray-400 rounded-lg text-p3 px-3">
-              Password
-            </div>
-            <div className="flex items-center justify-center h-[45px] w-[420px] text-light-color bg-primary-1 rounded-lg text-h6">
-              Masuk
-            </div>
+            <div className="form-input">Username</div>
+            <div className="form-input">Password</div>
+            <div className="button-input-1">Masuk</div>
             <p className="text-p4 font-medium">
               Belum punya akun?
               <span className="text-primary-1 font-bold"> Daftar</span>
